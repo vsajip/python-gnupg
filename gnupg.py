7762a7837c2743703f546569d1a8d82825b52cab
Yann Leboulanger <asterix@lagaule.org>
2016-12-08 09:43:07 +0100

    ithe output is always encoded as 'utf-8' when --with-column option is used

--- gnupg.py
+++ gnupg.py
@@ -1163,7 +1163,7 @@ class GPG(object):
         # Get the response information
         result = self.result_map[kind](self)
         self._collect_output(p, result, stdin=p.stdin)
-        lines = result.data.decode(self.encoding,
+        lines = result.data.decode('utf-8',
                                    self.decode_errors).splitlines()
         valid_keywords = 'pub uid sec fpr sub ssb sig'.split()
         for line in lines:
@@ -1252,7 +1252,7 @@ class GPG(object):
         # Get the response information
         result = self.result_map['search'](self)
         self._collect_output(p, result, stdin=p.stdin)
-        lines = result.data.decode(self.encoding,
+        lines = result.data.decode('utf-8',
                                    self.decode_errors).splitlines()
         valid_keywords = ['pub', 'uid']
         for line in lines:
