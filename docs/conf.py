e1e711cd08a38e323ba3790a53149cffb9952442
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2016-12-19 15:59:53 +0000

    Reverted sidebar config.

--- docs/conf.py
+++ docs/conf.py
@@ -130,14 +130,13 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
 
-html_sidebars = {
-    '**': [
-        'localtoc.html', 'globaltoc.html',  'relations.html',
-        'sourcelink.html', 'searchbox.html'
-    ],
-}
+# html_sidebars = {
+    # '**': [
+        # 'localtoc.html', 'globaltoc.html',  'relations.html',
+        # 'sourcelink.html', 'searchbox.html'
+    # ],
+# }
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
