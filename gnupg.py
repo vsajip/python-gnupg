af33bfa07aa8c8bfbd6aa739ed89dd5807e7c4a5
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2019-08-12 18:08:53 +0100

    Bumped version.

--- gnupg.py
+++ gnupg.py
@@ -32,7 +32,7 @@ Modifications Copyright (C) 2008-2019 Vinay Sajip. All rights reserved.
 A unittest harness (test_gnupg.py) has also been added.
 """
 
-__version__ = "0.4.5"
+__version__ = "0.4.6.dev0"
 __author__ = "Vinay Sajip"
 __date__  = "$12-Aug-2019 15:58:03$"
 
