3a735712bf67b244be1163b630dc37c53e5ef0a1
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2021-12-09 23:49:56 +0000

    Simplify tests (remove doctests, log test start/end in setUp/tearDown, better argument handling) and reapply fix for #161.

--- gnupg.py
+++ gnupg.py
@@ -699,6 +699,7 @@ class GenKey(object):
         self.gpg = gpg
         self.type = None
         self.fingerprint = None
+        self.status = None
 
     def __nonzero__(self):
         if self.fingerprint: return True
@@ -710,11 +711,14 @@ class GenKey(object):
         return self.fingerprint or ''
 
     def handle_status(self, key, value):
-        if key in ("WARNING", "ERROR"):  # pragma: no cover
+        if key in ('WARNING', 'ERROR'):  # pragma: no cover
             logger.warning('potential problem: %s: %s', key, value)
-        elif key == "KEY_CREATED":
+        elif key == 'KEY_CREATED':
             (self.type,self.fingerprint) = value.split()
-        elif key in ("PROGRESS", "GOOD_PASSPHRASE", "KEY_NOT_CREATED"):
+            self.status = 'ok'
+        elif key == 'KEY_NOT_CREATED':
+            self.status = key.replace('_', ' ').lower()
+        elif key in ('PROGRESS', 'GOOD_PASSPHRASE'):
             pass
         else:  # pragma: no cover
             logger.debug('message ignored: %s, %s', key, value)
